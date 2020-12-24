b2b7fcb33d2e93435aa88cf606d5f71044c38d4d
Paweł Pasternak <ppaste@gmail.com>
2020-12-24 10:28:49 +0100

    added push manager

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
-//Get notification permission from user
-Notification.requestPermission((status) => {
-    console.log("Notification prermission status: ", status)
-});
+function requestNotification() {
+    //Get notification permission from user
+    Notification.requestPermission((status) => {
+        console.log("Notification prermission status: ", status)
+    });
+}
 
 //Display test notification to user
 function displayNotification() {
     if(Notification.permission === 'granted') {
         navigator.serviceWorker.getRegistration().then((reg) => {
             let options = {
-                body: "Here is your notification body!",
+                body: "Notification from web page",
                 icon: "seaweed.png",
                 vibrate: [100,50,100],
                 data: {
@@ -17,13 +19,17 @@ function displayNotification() {
                     primaryKey: 1
                 },
                 actions: [
-                    {action:'explore',title:"Explore this new world"},
-                    {action:'close', title:"Close this notification"}
+                    {action:'explore',title:"Explore"},
+                    {action:'close', title:"Close"}
                 ]
 
             };
             reg.showNotification("Hello! This is your first notification!", options)
         })
+    } else if(Notification.permission === 'denied') {
+        console.log("No permission to show notifications")
+    } else {
+        requestNotification();
     }
 }
 
--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-console.log("Hello from serivceWorker")
+console.log("Hello from Service Worker");
+
+self.addEventListener('notificationclose', (e) => {
+    let notification = e.notification;
+    let primaryKey = notification.data.primaryKey;
+    console.log('Closed this notification: ' + primaryKey);
+});
+
+self.addEventListener('notificationclick', (e) => {
+    
+    let notification = e.notification;
+    let primaryKey = notification.data.primaryKey;
+    let action = e.action;
+
+    if(action === 'close') {
+        notification.close();
+        console.log("Notifcation closed.")
+    } else {
+        clients.openWindow('https://pawelpa.github.io/todoapp/');
+        console.log("Notification open url.")
+        notification.close();
+
+    }
+});
+//register push
+self.addEventListener('push', (e) => {
+    let options = {
+        body:"Notifiction generated from push",
+        icon:"seaweed.png",
+        data: {
+            primaryKey: 2,
+            dateOfArrival: Date.now()
+        },
+        actions: [
+            {action:'close', title:"Close"},
+            {action:'explore', title:'Explore the world'}
+        ]
+    };
+
+    e.waitUntil(
+        self.registration.showNotification("Hello world", optoins)
+    );
+});
